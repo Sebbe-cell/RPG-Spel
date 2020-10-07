c15161451c69d2da281264a4bf8353761c0e254c
Sebastian Johnsson <Cbas10an_@hotmail.com>
2020-10-07 14:06:37 +0200

    :)

--- RPGSpel/RPGSpel/MainGame/Program.cs
+++ RPGSpel/RPGSpel/MainGame/Program.cs
@@ -3,9 +3,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Media;
-using System.Security.Cryptography;
 using System.Net;
-using System.Data.SqlTypes;
 
 namespace PepsiMan
 {
