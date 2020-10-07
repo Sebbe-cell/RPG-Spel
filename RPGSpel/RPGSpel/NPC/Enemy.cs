bcca36dadd74ee9017594fe785ad56c28a95486e
Sebastian Johnsson <Cbas10an_@hotmail.com>
2020-10-08 00:13:55 +0200

    EnemyComment

--- RPGSpel/RPGSpel/NPC/Enemy.cs
+++ RPGSpel/RPGSpel/NPC/Enemy.cs
@@ -9,7 +9,7 @@ namespace PepsiMan
 
         // Deklarerar X & Y variablarna för att sedan i Draw() ge dom en plats i spelvärlden via X & Y variablarna
         // Initierar även EnemyMarker & Color (I detta fall ger vi dom en unik karaktär och färg.
-        public Enemy(int intX, int intY)
+        public Enemy(int intX, int intY) // Konstruktor
         {
             X = intX;
             Y = intY;
