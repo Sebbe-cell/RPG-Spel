5ff3d437f00ff7d4f2258b7050aad206cfca35df
Sebastian Johnsson <Cbas10an_@hotmail.com>
2020-10-08 00:10:34 +0200

    Kommentarar

--- RPGSpel/RPGSpel/NPC/Enemy.cs
+++ RPGSpel/RPGSpel/NPC/Enemy.cs
@@ -7,6 +7,8 @@ namespace PepsiMan
         public string EnemyMarker;
         public ConsoleColor EnemyColor;
 
+        // Deklarerar X & Y variablarna för att sedan i Draw() ge dom en plats i spelvärlden via X & Y variablarna
+        // Initierar även EnemyMarker & Color (I detta fall ger vi dom en unik karaktär och färg.
         public Enemy(int intX, int intY)
         {
             X = intX;
@@ -15,6 +17,7 @@ namespace PepsiMan
             EnemyColor = ConsoleColor.Red;
         }
 
+        // Ritar ut karaktären i spelvärlden, i detta fall en fiende.
         public void Draw()
         {
             Console.ForegroundColor = EnemyColor;
